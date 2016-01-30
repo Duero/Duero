3534a4df09c49e9974701c0b533bca7b1cd636e4
David Durika <durika.d@gmail.com>
2016-01-30 16:52:42 +0100

    prototyp: scheduleEditor update

--- client/modules/prototype/components/scheduleEditor.jsx
+++ client/modules/prototype/components/scheduleEditor.jsx
@@ -4,23 +4,37 @@ const ScheduleEditor = () => {
   return <div className="schedule schedule-editor">
     <h1 className="text-center">Pondelok</h1>
 
-    <div className="cleaner-box">
-      <div className="name">Helena F.</div>
-      <div className="buildings">
-        <div className="building-box">L. Stura 8 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Hradna 12 <span className="duration">(1:20)</span></div>
-        <div className="building-box">Pod kopcom 84 <span className="duration">(2:45)</span></div>
-      </div>
-      <div className="btn btn-link pull-right"><i className="fa fa-plus" /></div>
-    </div>
+    <div className="row">
+      <div className="col-xs-6 col-sm-9">
+        <div className="cleaner-box">
+          <div className="name">Helena F.</div>
+          <div className="buildings">
+            <div className="building-box">L. Stura 8 <span className="duration">(2:00)</span></div>
+            <div className="building-box">Hradna 12 <span className="duration">(1:20)</span></div>
+            <div className="building-box">Pod kopcom 84 <span className="duration">(2:45)</span></div>
+          </div>
+        </div>
 
-    <div className="cleaner-box">
-      <div className="name">Magda H.</div>
-      <div className="buildings">
-        <div className="building-box">Dlha 2 <span className="duration">(4:30)</span></div>
-        <div className="building-box">Kosicka 14 <span className="duration">(2:20)</span></div>
+        <div className="cleaner-box">
+          <div className="name">Magda H.</div>
+          <div className="buildings">
+            <div className="building-box">Dlha 2 <span className="duration">(4:30)</span></div>
+            <div className="building-box">Kosicka 14 <span className="duration">(2:20)</span></div>
+          </div>
+        </div>
+
+        <div className="cleaner-box">
+          <div className="name">Eva T.</div>
+          <div className="buildings">
+            <span className="italic color-soft">takto si nic nezarobi</span>
+          </div>
+        </div>
       </div>
-      <div className="btn btn-link pull-right"><i className="fa fa-plus" /></div>
+
+      <div className="col-xs-6 col-sm-3 unassigned-buildings">
+        <div className="building-box">Hradna 9 <span className="duration">(0:30)</span></div>
+        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
+      </div>
     </div>
 
   </div>
