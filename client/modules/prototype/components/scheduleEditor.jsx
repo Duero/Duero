62682adb2e5c402910420a1cf15a777377bdd689
tothradoslav <toth.radoslav@gmail.com>
2016-02-01 13:23:01 +0100

    Schedule editor design

--- client/modules/prototype/components/scheduleEditor.jsx
+++ client/modules/prototype/components/scheduleEditor.jsx
@@ -4,31 +4,7 @@ import ScheduleEditorComponent from '/client/modules/scheduleEditor/components/s
 
 const ScheduleEditor = () => {
 
-  const cleaners = [
-    {
-      cleaner: {name: 'Hanka S.'},
-      buildings: [
-        {name: 'Dlha 2', duration: '4:30'},
-        {name: 'Mlinska d. 5', duration: '2:10'},
-        {name: 'Hradna 134', duration: '0:45'}
-      ]
-    },
-
-    {
-      cleaner: {name: 'Magda V.'},
-      buildings: [
-        {name: 'Hlavna 42', duration: '7:30'}
-      ]
-    },
-
-    {
-      cleaner: {name: 'Eva P.'},
-      buildings: []
-    }
-
-  ];
-
-
+  const cleaners = [];
   return <ScheduleEditorComponent cleaners={cleaners} />
 };
 
--- client/modules/scheduleEditor/components/scheduleEditor.jsx
+++ client/modules/scheduleEditor/components/scheduleEditor.jsx
@@ -7,41 +7,98 @@ const ScheduleEditor = ({cleaners}) => {
   return <div className="schedule schedule-editor">
 
     <div className="row">
-      <div className="col-xs-6 col-sm-8">
-        <h1 className="text-center">Pondelok</h1>
-        {cleaners.map((item, index) => {
-          return <CleanerBox key={index} {...item} />
-        })}
+      <div className="col-xs-6 col-sm-8 col-md-10">
+    <h1 className="text-center">Pondelok</h1>
+
+    <div className="cleaner-box">
+        <dl className="dl-horizontal">
+          <dt>Hanka S.<br /><small><span className="text-muted">spolu 3:45</span></small></dt>
+          <dd>
+            <div className="btn-group">
+              <button type="button" className="btn btn-default btn-lg">T. G. Masaryka 55</button>
+              <button type="button" className="btn btn-default btn-lg"><i className="fa fa-times"></i></button>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn btn-default btn-lg">T. G. Masaryka 53</button>
+              <button type="button" className="btn btn-default btn-lg"><i className="fa fa-times"></i></button>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn btn-default btn-lg">T. G. Masaryka 57</button>
+              <button type="button" className="btn btn-default btn-lg"><i className="fa fa-times"></i></button>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn btn-default btn-lg">T. G. Masaryka 57</button>
+              <button type="button" className="btn btn-default btn-lg"><i className="fa fa-times"></i></button>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn btn-default btn-lg">T. G. Masaryka 57</button>
+              <button type="button" className="btn btn-default btn-lg"><i className="fa fa-times"></i></button>
+            </div>
+            <div className="btn-group">
+              <button type="button" className="btn btn-default btn-lg">T. G. Masaryka 57</button>
+              <button type="button" className="btn btn-default btn-lg"><i className="fa fa-times"></i></button>
+            </div>
+          </dd>
+      </dl>
+    </div>
+    <div className="cleaner-box">
+        <dl className="dl-horizontal">
+          <dt>Janka S.<br /><small><span className="text-muted">spolu 0:00</span></small></dt>
+          <dd>
+          </dd>
+      </dl>
+    </div>
+
       </div>
 
-      <div className="col-xs-6 col-sm-4 unassigned-buildings">
-        <div className="gradient" />
-        <div className="building-box">Hradna 9 <span className="duration">(0:30)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
-        <div className="building-box">Puskinova 13 <span className="duration">(2:00)</span></div>
+      <div className="col-xs-6 col-sm-4 col-md-2">
+        <div className="panel panel-primary">
+          <div className="panel-heading">
+            <h3 className="panel-title">Objekty</h3>
+          </div>
+          <div className="panel-body">
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+              <button type="button" className="btn btn-default btn-block">Cras justo odio</button>
+              <button type="button" className="btn btn-default btn-block">Dapibus ac facilisis in</button>
+              <button type="button" className="btn btn-default btn-block">Morbi leo risus</button>
+              <button type="button" className="btn btn-default btn-block">Porta ac consectetur ac</button>
+              <button type="button" className="btn btn-default btn-block">Vestibulum at eros</button>
+          </div>
+        </div>
       </div>
     </div>
 
